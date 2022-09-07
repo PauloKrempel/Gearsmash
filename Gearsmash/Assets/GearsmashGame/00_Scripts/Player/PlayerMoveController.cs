0a5a5c753e4c038995062d5711cd7da1acdb2dff
Paulo Krempel <pauloh.krempel@gmail.com>
2022-09-07 12:45:57 -0300

    Edit

--- Gearsmash/Assets/GearsmashGame/00_Scripts/Player/PlayerMoveController.cs
+++ Gearsmash/Assets/GearsmashGame/00_Scripts/Player/PlayerMoveController.cs
@@ -6,17 +6,38 @@ public class PlayerMoveController : MonoBehaviour
 {
     [Header("Player Animation")]
     [HideInInspector][SerializeField] Animator anim;
+
+    [Header("Player Moving")]
+    private Rigidbody2D rb2d;
+    private float horizontal;
+    private float vertical;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float jumpForce = 300f;
+
+
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.W))
         {
-            anim.SetTrigger("jump");
+            anim.Play("concept_JumpPlayer");
+            
         }
+    }
+    public void JumpPlayer()
+    {
+        rb2d.AddForce(Vector2.up * jumpForce);
+    }
+    private void FixedUpdate() {
+        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
     }
 }
