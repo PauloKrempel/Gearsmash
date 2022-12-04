9bb460ff9cad6378a5b66e70c70aa9b1ef13e219
Paulo Krempel <pauloh.krempel@gmail.com>
2022-12-04 17:24:55 -0300

    timeline

--- Gearsmash/Assets/AudioManager.cs
+++ Gearsmash/Assets/AudioManager.cs
@@ -1,26 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    public Button[] buttonsInterface;
+    public GameObject[] buttonsInterface;
+
+    public AudioSource tutorialP1;
+    public AudioSource tutorialP2;
+    
+
 
     public bool isPlay = false;
+
+    public bool finalTutorialP1 = false;
     // Start is called before the first frame update
     void Start()
     {
+        //tutorialP1.Play();
+        if (!isPlay)
+        {
+            foreach (GameObject btn in buttonsInterface)
+            {
+                btn.SetActive(false);
+
+            }
+        }
         
     }
     void Update()
     {
-        while (!isPlay)
+        if(isPlay && !finalTutorialP1)
         {
-            foreach (var btn in buttonsInterface)
-            {
-                btn.interactable = false;
-            }
+            buttonsInterface[0].SetActive(true); //esquerda
+            buttonsInterface[1].SetActive(true); //direita
+            buttonsInterface[2].SetActive(true); //cima
+            buttonsInterface[4].SetActive(true); //Ataque basico
+            
+            // foreach (GameObject btn in buttonsInterface)
+            // {
+            //     btn.SetActive(true);
+            //     
+            //
+            // }
+
+            finalTutorialP1 = true;
+            Debug.LogError("Hora de ativar");
         }
+    }
+
+    public void StartMove()
+    {
+        isPlay = true;
+        Debug.LogError("Entrou no tutorial");
     }
 }
---
+++ Gearsmash/Assets/GearsmashGame/00_Scripts/Tutorial/TutorialPU.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TutorialPU : MonoBehaviour
+{
+    
+    [Header("Caracteristicas Raycast")]
+    [SerializeField][Range(0f, 15f)][Tooltip("Range do raio que atingira o enemy")] float rayLength = 10f;
+    public bool FoundEnemy = false;
+    private LayerMask LayerEnemeyTutorial;
+    private bool FinishedTutorialEnemy = false;
+    public GameObject TutorialEnemy;
+    
+
+    [Header("Audio Tutorial")] public AudioSource aviso;
+    public PlayableDirector Director;
+
+    private void Awake()
+    {
+        // Director = GetComponent<PlayableDirector>();
+        Director.played += Director_Played;
+        Director.stopped += Director_Stoped;
+    }
+
+    private void Start()
+    {
+        LayerEnemeyTutorial = LayerMask.GetMask("EnemyTutorial");
+        
+    }
+
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.right, rayLength, LayerEnemeyTutorial);
+        if (hit.collider != null && !FoundEnemy && !FinishedTutorialEnemy)
+        {
+            StartTimeline();
+            Debug.LogError("Encontrei..");
+            FinishedTutorialEnemy = true;
+            //Time.timeScale = Time.timeScale == 1.0f ? 0.0f : 1.0f;
+        }
+
+        if (FinishedTutorialEnemy)
+        {
+            this.enabled = false;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Vector3 dir = transform.TransformDirection(Vector3.right) * rayLength;
+        Gizmos.DrawRay(transform.position, dir);
+    }
+
+    public void Despausar()
+    {
+        Time.timeScale = Time.timeScale == 1.0f ? 0.0f : 1.0f;
+    }
+
+    void Director_Played(PlayableDirector obj)
+    {
+        TutorialEnemy.SetActive(true);
+    }
+
+    void Director_Stoped(PlayableDirector obj)
+    {
+        TutorialEnemy.SetActive(false);
+    }
+
+    public void StartTimeline()
+    {
+        Director.Play();
+    }
+}
