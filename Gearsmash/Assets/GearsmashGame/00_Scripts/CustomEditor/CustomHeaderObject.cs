d73b7dc4961d0e35ed387658300461afc93fd7f6
Paulo Krempel <pauloh.krempel@gmail.com>
2022-12-07 23:28:54 -0300

    android

--- Gearsmash/Assets/GearsmashGame/00_Scripts/CustomEditor/CustomHeaderObject.cs
+++ Gearsmash/Assets/GearsmashGame/00_Scripts/CustomEditor/CustomHeaderObject.cs
@@ -7,7 +7,7 @@ public class CustomHeaderObject : MonoBehaviour
 {
     public Color textColor = Color.white;
     public Color backgroundColor = Color.red;
-    private void OnValidate() {
-        EditorApplication.RepaintHierarchyWindow();
-    }
+    // private void OnValidate() {
+    //     EditorApplication.RepaintHierarchyWindow();
+    // }
 }
